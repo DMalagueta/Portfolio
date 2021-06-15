8ab733109d49b2d2db5f564d483eb7a2a9d16f84
Diogo Malagueta <mrmalagueta@gmail.com>
2021-06-16 00:31:37 +0100

    make it better

---
+++ precache-manifest.43d495a86034150233680957b2e0a736.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589385891448f1fa8f1a6476141223f9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "15e8fb1b74b279718a47",
+    "url": "/Portfolio/static/css/main.22337fdc.chunk.css"
+  },
+  {
+    "revision": "fd881a44bef3f2d97cad",
+    "url": "/Portfolio/static/js/2.b22793ad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.b22793ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e8fb1b74b279718a47",
+    "url": "/Portfolio/static/js/main.f46d06d6.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/Portfolio/static/media/firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "7dffd9f3befc41d7496ce325f3055084",
+    "url": "/Portfolio/static/media/html.7dffd9f3.svg"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/Portfolio/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/Portfolio/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/Portfolio/static/media/nodejs.94cafb0d.svg"
+  },
+  {
+    "revision": "5d1cdce6c8bbb006ac6cefb8e1642877",
+    "url": "/Portfolio/static/media/react-query.5d1cdce6.svg"
+  },
+  {
+    "revision": "d7396d6757fd902f37f8d5b15dcac37d",
+    "url": "/Portfolio/static/media/react-router.d7396d67.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/Portfolio/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/Portfolio/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "3323dd0202edcaf8d4392e994668959c",
+    "url": "/Portfolio/static/media/tailwindcss.3323dd02.svg"
+  }
+]);
